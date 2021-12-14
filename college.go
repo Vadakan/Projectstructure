8c7647354effdc8e0aba3d2903bbc936659fdade
Sundar Munichamy <msundarmunichamy@gmail.com>
2021-12-14 23:28:51 +0530

    Init college and added AddDept and GetDept functions

--- college.go
+++ college.go
@@ -3,3 +3,25 @@ package main
 type college struct {
 	department []Department
 }
+
+func Initcollege() *college {
+	return &college{}
+}
+
+func (c *college) Adddepartment(name string) {
+	if name == "Mechanical" {
+		c.department = append(c.department, getMech(10, name))
+	} else if name == "CSE" {
+		c.department = append(c.department, getComputerscience(name, 20))
+	}
+
+}
+
+func (c *college) getDepartment(name string) Department {
+	for _, department := range c.department {
+		if name == department.GetName() {
+			return department
+		}
+	}
+	return &NullDepartment{}
+}
